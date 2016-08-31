16d0da65aa35ea78128c12f2e57bfde101205442
Dana <danacvtt@hotmail.com>
2016-08-31 11:33:43 -0300

    Add files via upload

---
+++ 2016AD_G04_Repositorio/src/main/java/com/ADG04/Repositorio/App.java
@@ -0,0 +1,13 @@
+package com.ADG04.Repositorio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ 2016AD_G04_Repositorio/src/main/java/com/ADG04/Repositorio/Interfaces/InterfazRemotaDistribucionPaquetes.java
@@ -0,0 +1,10 @@
+package com.ADG04.Repositorio.Interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+
+public interface InterfazRemotaDistribucionPaquetes extends Remote{
+	public static final String url = "localhost/DistribucionPaquetesRMI";
+}
